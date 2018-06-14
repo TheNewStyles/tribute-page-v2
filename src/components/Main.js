646a90975a87025a720a81844dbdbab6e74b1d7c
TheNewStyles <tyler.peterson@utah.edu>
2018-06-14 17:22:15 -0500

    initial commit using create-react-app

---
+++ src/components/Main.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export class Main extends Component {  
+    render() {
+      return (
+       <div>
+        <h1>Main</h1>
+       </div>
+      );
+    }
+  }
