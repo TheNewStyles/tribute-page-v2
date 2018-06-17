aa98b35094c13d30967fc4d4ff58362ddb98d220
TheNewStyles <tyler.peterson@utah.edu>
2018-06-17 13:04:57 -0500

    add jest enzyme for testing react dom

--- src/__tests__/Header.test.js
+++ src/__tests__/Header.test.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import Header from '../components/Header';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Header />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('<Header />', () => {
+
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Header />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it('find title', () => {
+    const wrapper = shallow(<Header />);
+    const title = <h1 id="title" className="flex-item">Bill Gates</h1>;
+    expect(wrapper).toContainReact(title);
+  });
+
 });
